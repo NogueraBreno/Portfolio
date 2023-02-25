f5550728c690b862cf950b86c300f319afc8c6f8
BreNo <nogueirabreno@rocketmail.com>
2023-02-25 02:13:18 -0400

    Att

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
+
 var slideIndex = 1;
 showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function plusDivsSkills(n) {
+  showDivsSkills(slideIndex += n);
 }
 
-function showDivs(n) {
+function showDivsSkills(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
+  var x = document.getElementsByClassName("mySlidesSkills");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
@@ -16,43 +17,20 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";  
 }
 
-
-
-function isChecked(){
-const body = document.body
-const text1 = document.getElementById("tl-text")
-const text2 = document.getElementById("bot-text")
-const logow = document.getElementById("top-logo-white")
-const logob = document.getElementById("top-logo-black")
-const aba = document.getElementById("aba-topo")
-
-
-
-	if(document.getElementById("switcher").checked){
-
-		logow.style.display = "none";
-		logob.style.display = "inline";
-		body.classList.add("dark");
-		aba.classList.add("aba-white");
-		aba.classList.remove("aba-black");
-		text1.classList.add("dark-content-box");
-		text2.classList.add("dark-content-box");
-
-	}
-	else {
-
-		logow.style.display = "inline";
-		logob.style.display = "none";
-		body.classList.remove("dark");
-		aba.classList.add("aba-black")
-		aba.classList.remove("aba-white")
-		text1.classList.remove("dark-content-box");
-		text2.classList.remove("dark-content-box");
-
-	}
-
+function plusDivsCerts(n) {
+  showDivsCerts(slideIndex += n);
 }
 
+function showDivsCerts(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlidesCerts");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
 
 function showAll(){
 
@@ -65,3 +43,4 @@ function showAll(){
 		showbutton.style.display = "none";  
 
 }
+
